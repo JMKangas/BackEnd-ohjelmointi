e806f2964a3f6f8a02a6c662b2e37c56f8b373de
JMKangas <80038481+JMKangas@users.noreply.github.com>
2022-11-24 18:34:46 +0200

    more assingments

---
+++ Java-MasterClass/Pound to kilogram/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //Pound to kilogram convertor
+        double pound = 200d;
+        double kilogram = pound * 0.45359237d;
+        System.out.println(pound + " pound is equal to " + kilogram + " kilogram");
+    }
+}
